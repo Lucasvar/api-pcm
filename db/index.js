0c554848eed1fb73bf4d9b3b874835a521af0577
Lucas <lucasgvargas96@gmail.com>
2019-12-31 12:03:12 -0300

    added database

---
+++ db/index.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+// your credentials
+
+const pool = new Pool({
+  connectionString:  process.env.DB_URL
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    const start = Date.now()
+    return pool.query(text, params, (err, res) => {
+      const duration = Date.now() - start
+      console.log('executed query', { text,params,  duration })
+      callback(err, res)
+    })
+  },
+  getClient: (callback) => {
+    pool.connect((err, client, done) => {
+      callback(err, client, done)
+    })
+  }
+}
