4eb4367cefead1077668076979e0247962fbe0d2
Lucas <lucasgvargas96@gmail.com>
2020-03-27 20:16:37 -0300

    signup now creates an entry in the users and accounts db at registration

--- routes/signup.js
+++ routes/signup.js
@@ -9,17 +9,28 @@ const router = Router()
 router.post('/', function(req, res, next) {
 	const password = req.body.password
 	const username = req.body.username
-	const text = 'INSERT INTO accounts(username, password) VALUES ($1, $2);'
-	const values = [username]
+	values = [username]
 
 	bcrypt.hash(password, saltRounds, function(err, hash) {
 		values.push(hash)
-		db.query(text, values, (err, data) => {
+		db.query('INSERT INTO accounts(username, password) VALUES ($1, $2) RETURNING id;', values, (err, data) => {
 			if (err) {
-				return next(err)
+				res.status(400).json({"error":"username taken"})
 			}
+			else {
 			//res.location(`/materias/${materia.id}`)
-			res.send(`Account ${username} created successfully.`)
+			console.log(data.rows[0].id)
+				values = [data.rows[0].id,req.body.nya, req.body.email, req.body.id_career]
+				db.query('INSERT INTO users(id, nya, mail, id_career) VALUES ($1, $2, $3, $4);', values, (err, data) => {
+					if (err) {
+						res.status(400).json({"error":"email taken"})
+					}
+					else {
+					//res.location(`/materias/${materia.id}`)
+					res.send(`Account ${username} created successfully.`)
+					}
+				})
+			}
 		})
 	});
 
